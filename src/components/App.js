7ac183e65fe4782d1a6f32440227065b4c751223
radwa <mail2radwa@gmail.com>
2021-05-02 11:15:53 +0200

    chore: Initial app data

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 
 class App extends Component {
+  
+  componentDidMount() {
+    /* When component mounts load the initial data 
+       by dispatching the handleInitialData() action creator
+    */
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +20,7 @@ class App extends Component {
   }
 }
 
-export default  App;
+/* Using the connect() function to make container component
+   to read state from the store and dispatch actions
+*/
+export default connect()(App) ;
