dcf6e34afb10a4e79d1a271a245c1f4366ce8a3e
radwa <mail2radwa@gmail.com>
2021-05-04 10:12:49 +0200

    feat: Build MainRouter component

--- src/components/MainRouter.js
+++ src/components/MainRouter.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import LeaderBoard from './LeaderBoard';
+import NewQuestion from './NewQuestion';
 class MainRouter extends Component {
   render() {
     return (
       <div>
-        Main Router
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/add' component={NewQuestion} />
+          <Route path='/leaderboard' component={LeaderBoard} />
+        </Switch>
       </div>
     )
   }
